3f8551accc7e56eae3639668ce3c6db4fc2cda90
Xearta <rsmcwow@gmail.com>
2020-10-12 16:07:54 -0400

    Created the Post component layout

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Feed.css';
 import MessageSender from './MessageSender';
+import Post from './Post';
 import StoryReel from './StoryReel';
 
 const Feed = () => {
@@ -8,6 +9,21 @@ const Feed = () => {
     <div className='feed'>
       <StoryReel />
       <MessageSender />
+
+      <Post
+        profilePic='https://avatars0.githubusercontent.com/u/1683791?s=460&u=abccb06c384728e7fda2185195a8389600db7c76&v=4'
+        message='WOW this works!'
+        timestamp='This is a timestamp'
+        username='Tanner Townsend'
+        image='https://code.org/shared/images/social-media/codeorg2019_social.png'
+      />
+
+      <Post
+        profilePic='https://avatars0.githubusercontent.com/u/1683791?s=460&u=abccb06c384728e7fda2185195a8389600db7c76&v=4'
+        message='WOW this works!'
+        timestamp='This is a timestamp'
+        username='Tanner Townsend'
+      />
     </div>
   );
 };
