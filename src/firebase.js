9008193b86992a0181c229f44e0597e39c0c1caa
Xearta <rsmcwow@gmail.com>
2020-10-12 13:56:39 -0400

    Created the header with material ui icons

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCjDN3niN5PJkQJ-KAX_UPKDYMQwUx_c9E',
+  authDomain: 'fb-clone-f6346.firebaseapp.com',
+  databaseURL: 'https://fb-clone-f6346.firebaseio.com',
+  projectId: 'fb-clone-f6346',
+  storageBucket: 'fb-clone-f6346.appspot.com',
+  messagingSenderId: '201143195179',
+  appId: '1:201143195179:web:579637713e9f4054e83c6e',
+  measurementId: 'G-4290W0580C',
+};
